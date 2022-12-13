72d626a8a66d9e8dd6d9f6a1a0717296b77ba6c5
Zion <zionperez@gmail.com>
2022-12-12 20:00:30 -0700

    Implement potato-loop: tilling soil, planting seeds, eating potatoes, repeat.

--- game.py
+++ game.py
@@ -1,367 +1,317 @@
 import time
 import keyboard
-from sys import exit
-import pickle
 import os
+import copy
+from enum import Enum
+# from sys import exit
 
-current_level = 0
-all_levels = [0, 27, 54, 81, 108, 135, 162, 189, 216]
+# constants
+INVENTORY = "inventory"
+WALL = "#"
+HOE  = "h"
+BLANK = " "
+PLAYER = "@"
+SHIP = "S"
+SEED = "."
+SEEDLING = ","
+SPROUT = "i"
+POTATO = "p"
+TILLED_SOIL = "t"
+ITEMS = [HOE, POTATO]
+STACKABLE = [SEED, POTATO]
 
-row_1 = []
-row_2 = []
-row_3 = []
-row_4 = []
-row_5 = []
-row_6 = []
-row_7 = []
-row_8 = []
-row_9 = []
-row_10 = []
-row_11 = []
-row_12 = []
-row_13 = []
-row_14 = []
-row_15 = []
-row_16 = []
-row_17 = []
-row_18 = []
-row_19 = []
-row_20 = []
-row_21 = []
-row_22 = []
-row_23 = []
-row_24 = []
+WORD_ITEM = {
+    SEED : "seed",
+    HOE  : "hoe",
+    SEEDLING : "seedling",
+    SPROUT : "sprout",
+    POTATO : "potato",
+    TILLED_SOIL : "tilled soil"
+}
 
-level_y = [row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9, row_10, row_11, row_12, row_13, row_14, row_15, row_16, row_17, row_18, row_19, row_20, row_21, row_22, row_23, row_24]
+# Stats
+POTATO_GAINS = 5
+MAX_HEALTH = 100
+MAX_SATIATION = 100
+HUNGER_INTERVAL = 30
 
-enemy_x = 65
-enemy_y = 20
-e_last_x = 69
-e_last_y = 21
-player_x = 6
-player_y = 3
-last_x = 6
-last_y = 3
-lives = 5
-score = 0
-loop = True
-saved_variables = []
+# Mesasges
+SHIP_OUT_OF_SEEDS = "Ship currently has 0 seeds. Try again later."
+MESSAGE_LIMIT = 30
 
-def print_level():
-    print(*row_1, sep='')
-    print(*row_2, sep='')
-    print(*row_3, sep='')
-    print(*row_4, sep='')
-    print(*row_5, sep='')
-    print(*row_6, sep='')
-    print(*row_7, sep='')
-    print(*row_8, sep='')
-    print(*row_9, sep='')
-    print(*row_10, sep='')
-    print(*row_11, sep='')
-    print(*row_12, sep='')
-    print(*row_13, sep='')
-    print(*row_14, sep='')
-    print(*row_15, sep='')
-    print(*row_16, sep='')
-    print(*row_17, sep='')
-    print(*row_18, sep='')
-    print(*row_19, sep='')
-    print(*row_20, sep='')
-    print(*row_21, sep='')
-    print(*row_22, sep='')
-    print(*row_23, sep='')
-    print(*row_24, sep='')
-   
-def clear_level():
-    row_1.clear()
-    row_2.clear()
-    row_3.clear()
-    row_4.clear()
-    row_5.clear()
-    row_6.clear()
-    row_7.clear()
-    row_8.clear()
-    row_9.clear()
-    row_10.clear()
-    row_11.clear()
-    row_12.clear()
-    row_13.clear()
-    row_14.clear()
-    row_15.clear()
-    row_16.clear()
-    row_17.clear()
-    row_18.clear()
-    row_19.clear()
-    row_20.clear()
-    row_21.clear()
-    row_22.clear()
-    row_23.clear()
-    row_24.clear()
+# globals
+clock = 0
+world = []
+item_map = {} # { (x, y) : 'item' }
+player = {
+    "pos" : (),
+    "inventory" : {},
+    "equipped" : None,
+    "health" : MAX_HEALTH,
+    "hunger" : MAX_SATIATION
+}
+ship_pos = ()
+feedback_messages = []
+seed_tracker = [] # [ (x, y, clock) ]
+seed_stages = [SEED, SEEDLING, SPROUT, POTATO]
+seed_growth_time = 100
+last_observed_item = None
 
-def new_level():
-    global player_x
-    global player_y
-    global enemy_x
-    global enemy_y
-    enemy_x = 65
-    enemy_y = 20
-    player_x = 6
-    player_y = 3
-    clear_level()
-    byte = all_levels[current_level]
-    range_value = byte
-    range_value2 = byte + 24
-    row = 0
-    while byte in range(range_value, range_value2):
-        read_file = open('levels.txt', 'r')
-        read_line = read_file.readlines()
-        final_answer = read_line[byte]
-        byt = 0
-        first = 0
-        second = 1
-        while byt in range(0, 72):
-            line_y = level_y[row]
-            line_y.append(final_answer[first:second])
-            first = first + 1
-            second = second + 1
-            byt = byt + 1
-        byte = byte + 1
-        row = row + 1
-    row_1.append("  Score: ")
-    row_1.append("")
-    row_2.append("  Lives: ")
-    row_2.append("")
+# def is_matching(coord1, coord2):
+#     assert len(coord1) == len(coord2) == 2
+#     if coord1[0] != coord2[0]:
+#         return False
+#     if coord1[1] != coord2[1]:
+#         return False
+#     return True
 
-def save():
-    global saved_variables
-    global score
-    global lives
-    global player_x
-    global player_y
-    global enemy_x
-    global enemy_y
-    global current_level
-    saved_variables = [score, lives, current_level]
-    with open('chase_save.txt', 'wb') as fp:
-        pickle.dump(saved_variables, fp)
-    print('saved to "chase_save.txt"')
-    time.sleep(1)
-    pause_menu(0)
-   
-def open_save():
-    global saved_variables
-    global score
-    global lives
-    global player_x
-    global player_y
-    global enemy_x
-    global enemy_y
-    global current_level
-    print('opening "chase_save.txt"')
-    time.sleep(1)
-    with open('chase_save.txt', 'rb') as fp:
-        saved_variables = pickle.load(fp)
-    score = saved_variables[0]
-    lives = saved_variables[1]
-    current_level = saved_variables[2]
-    pause_menu(0)
-   
-   
-   
-def pause_menu(print_pause):
-    if print_pause == 1:
-        print(" _ __   __ _ _   _ ___  ___ ")
-        print("| '_ \ / _` | | | / __|/ _ \ ")
-        print("| |_) | (_| | |_| \__ \  __/")
-        print("| .__/ \__,_|\__,_|___/\___|")
-        print("|_|")
-        print("")
-    print('"c" to resume, "q" to quit, "s" to save, "o" to open an existing game')
-    while loop:
-        if keyboard.is_pressed('c'):
-            return
-        if keyboard.is_pressed('q'):
-            exit()
-        if keyboard.is_pressed('s'):
-            save()
-        if keyboard.is_pressed('o'):
-            open_save()
 
-def take_inputs():
-    move_up = True
-    move_down = True
-    move_right = True
-    move_left = True
-    e_move_up = True
-    e_move_down = True
-    e_move_right = True
-    e_move_left = True
-    global last_y
-    global last_x
-    global player_y
-    global player_x
-    global e_last_y
-    global e_last_x
-    global enemy_y
-    global enemy_x
-    global loop
-    global score
-    global lives
-    last_x = player_x
-    last_y = player_y
-    which_y = level_y[player_y + 1]
-    y_up = which_y[player_x]
-    which_y = level_y[player_y - 1]
-    y_down = which_y[player_x]
-    which_y = level_y[player_y]
-    x_right = which_y[player_x + 1]
-    x_left = which_y[player_x - 1]
-    if y_up == "#":
-        move_up = False
-    if y_down == "#":
-        move_down = False
-    if x_right == "#":
-        move_right = False
-    if x_left == "#":
-        move_left = False
-       
-    if keyboard.is_pressed('p'):
-        pause_menu(1)
-       
-    if move_down == True:
-        if keyboard.is_pressed('w'):
-            player_y = player_y - 1
-            if y_down == "^" or y_down == ">" or y_down == "v" or y_down == "<":
-                score = 0
-                lives = lives - 1
-                new_level()
-            if y_down == "c":
-                score = score + 1
-            if y_down == "H":
-                lives = lives + 1
-    if move_up == True:
-        if keyboard.is_pressed('s'):
-            player_y = player_y + 1
-            if y_up == "^" or y_up == ">" or y_up == "v" or y_up == "<":
-                score = 0
-                lives = lives - 1
-                new_level()
-            if y_up == "c":
-                score = score + 1
-            if y_up == "H":
-                lives = lives + 1
-    if move_left == True:
-        if keyboard.is_pressed('a'):
-            player_x = player_x - 1
-            if x_left == "^" or x_left == ">" or x_left == "v" or x_left == "<":
-                score = 0
-                lives = lives - 1
-                new_level()
-            if x_left == "c":
-                score = score + 1
-            if x_left =="H":
-                lives = lives + 1
-    if move_right == True:
-        if keyboard.is_pressed('d'):
-            player_x = player_x + 1
-            if x_right == "^" or x_right == ">" or x_right == "v" or x_right == "<":
-                score = 0
-                lives = lives - 1
-                new_level()
-            if x_right == "c":
-                score = score + 1
-            if x_right == "H":
-                lives = lives + 1
-       
-    e_last_x = enemy_x
-    e_last_y = enemy_y
-    which_y = level_y[enemy_y + 1]
-    y_up = which_y[enemy_x]
-    which_y = level_y[enemy_y - 1]
-    y_down = which_y[enemy_x]
-    which_y = level_y[enemy_y]
-    x_right = which_y[enemy_x + 1]
-    x_left = which_y[enemy_x - 1]
-    if y_up == "#" or y_up == "^" or y_up == ">" or y_up == "<" or y_up == "v" or y_up == "+" or y_up == "c" or y_up == "H":
-        e_move_up = False
-    if y_down == "#" or y_down == "^" or y_down == ">" or y_down == "<" or y_down == "v" or y_down == "+" or y_down == "c" or y_down == "H":
-        e_move_down = False
-    if x_right == "#" or x_right == "^" or x_right == ">" or x_right == "<" or x_right == "v" or x_right == "+" or x_right == "c" or x_right == "H":
-        e_move_right = False
-    if x_left == "#" or x_left == "^" or x_left == ">" or x_left == "<" or x_left == "v" or x_left == "+" or x_left == "c" or x_left == "H":
-        e_move_left = False
-     
-    if e_move_up == True and player_y > enemy_y:
-        enemy_y = enemy_y + 1
-    if e_move_down == True and player_y < enemy_y:
-        enemy_y = enemy_y - 1
-    if e_move_right == True and player_x > enemy_x:
-        enemy_x = enemy_x + 1
-    if e_move_left == True and player_x < enemy_x:
-        enemy_x = enemy_x - 1
-     
-def evaluate():
-    global player_x
-    global player_y
-    global loop
-    global current_level
-    global score
-    global lives
-    if player_x == enemy_x and player_y == enemy_y:
-        score = 0
-        lives = lives - 1
-        new_level()
-    if lives == 0:
-        print("You Win!")
-        time.sleep(1)
-        play_again = input("press enter to replay")
-        if play_again == '':
-            current_level = 0
-            score = 0
-            lives = 5
-            new_level()
+def parse_tile(x, y, tile, world_row):
+    global item_map
+    global ship_pos
+    if tile == PLAYER:
+        player["pos"] = (x, y)
+        world_row.append(BLANK)
+    elif tile == SHIP:
+        ship_pos = (x, y)
+        world_row.append(BLANK)
+    elif tile in ITEMS:
+        item_map[(x, y)] = tile
+        world_row.append(BLANK)
+    else:                    
+        world_row.append(tile)
+
+
+def load_world(filename):
+    global world
+    with open(filename, 'r', encoding='utf8') as handle:
+        for y, line in enumerate(handle):
+            line = line.strip()
+            world_row = []
+            for x, tile in enumerate(line):
+                parse_tile(x, y, tile, world_row)                
+            world.append(world_row)
+    for row in world:
+        print(*row, sep='')
+    # input("Press enter to continue...")
+
+
+def player_action():
+    global world    
+    global seed_tracker
+
+    # Ignore 'spacebar' action if player in ship
+    if is_player_in_ship():
+        return
+
+    x, y = player["pos"]
+    if player["equipped"] == HOE and world[y][x] == BLANK:
+        world[y][x] = TILLED_SOIL
+        print_feedback("You tilled the earth and made the land fertile.")
+    elif player["equipped"] == SEED and world[y][x] == TILLED_SOIL:
+        world[y][x] = SEED
+        seed_tracker.append( (x, y, clock) )
+        print_feedback("You planted the mighty seed in fertile soil.")
+        player["inventory"][SEED] -= 1
+        if player["inventory"][SEED] == 0:
+            del player["inventory"][SEED]
+    elif player["equipped"] == SEED and world[y][x] == BLANK:
+        print_feedback("The soil is not tilled. It would be a shame to waste a seed..")
+
+
+def is_player_in_ship():
+    return ship_pos == player["pos"]
+
+
+def print_ship_menu():
+    print("SHIP MENU: Press [1] to get seeds. Press [2] to get egg.")    
+
+
+def print_feedback(message):
+    global feedback_messages
+    feedback_messages.append(( message, clock ))
+
+
+def try_dispense_seeds():
+    global feedback_messages
+    global player
+    if SEED in player["inventory"]:
+        print_feedback(SHIP_OUT_OF_SEEDS)        
+        return
+    player["inventory"][SEED] = 10
+
+
+def try_equip_item(keypressed):
+    global player    
+    index = 0
+    items = list(player["inventory"].keys())
+    if (keypressed) > len(items):
+        return
+    item = items[keypressed - 1]    
+    if item == POTATO:
+        print_feedback("You feasted on the super potato satisfied your hunger")
+        player["hunger"] = MAX_SATIATION
+    else:
+        player["equipped"] = item
+        print_feedback("You equipped the mighty " + WORD_ITEM[item])
+    index += 1
+
+
+def print_inventory():
+    inventory_output = "Inventory: "
+    hotkey = 1
+    for item, count in player["inventory"].items():
+        item_word = WORD_ITEM[item]
+        if count > 1:
+            inventory_output += "[" + str(hotkey) + "] " + item_word + " x" + str(count) + "   "
         else:
-            time.sleep(1)
-            print("thank you for playing")
-            time.sleep(1)
-            exit()
-    if player_x == 65 and player_y == 20:
-        current_level = current_level + 1
-        if current_level == 9:
-            print("You Lose!")
-            time.sleep(1)
-            play_again = input('press enter to replay')
-            if play_again == '':
-                current_level = 0
-                new_level()
-            else:
-                time.sleep(1)
-                print("thank you for playing")
-                time.sleep(1)
-                exit()
-        new_level()
-       
-def replace():
-    which_last_y = level_y[last_y]
-    which_last_y[last_x] = '_'
-    which_last_y = level_y[e_last_y]
-    which_last_y[e_last_x] = '_'
-    which_y = level_y[player_y]
-    which_y[player_x] = 'O'
-    which_y = level_y[enemy_y]
-    which_y[enemy_x] = '*'
-    which_y = level_y[20]
-    which_y[65] = '+'
-    row_1[73] = score
-    row_2[73] = lives
+            inventory_output += "[" + str(hotkey) + "] " + item_word + "   "
+        hotkey += 1
+    print(inventory_output)
+    equipped = None
+    if player["equipped"]:
+        equipped = WORD_ITEM[player["equipped"]]
+    print("Equipped:", equipped)
 
-new_level()
 
-while True:    
-    take_inputs()
-    evaluate()
-    print_level()
-    replace()    
-    time.sleep(0.13)
-    os.system('cls')
+def print_player_stats():
+    health = player["health"]
+    hunger = player["hunger"]
+    print(f"Health {health} : Hunger {hunger}")
+
+
+def observe_world():
+    global last_observed_item
+    
+    x, y = player["pos"]
+    item = world[y][x]
+    if last_observed_item == item:
+        return
+    if item not in WORD_ITEM:
+        return 
+    word_item = WORD_ITEM[item]
+    if item == TILLED_SOIL:
+        print_feedback(f"This looks like {word_item}")
+    else:
+        print_feedback(f"This looks like a {word_item}")
+    last_observed_item = item
+
+def process():
+    global player_in_ship
+
+    observe_world()
+
+    # user input
+    x, y = player["pos"]
+    if keyboard.is_pressed('w') and world[y-1][x] != WALL:
+        player["pos"] = (x, y-1)
+    if keyboard.is_pressed('a') and world[y][x-1] != WALL:
+        player["pos"] = (x-1, y)
+    if keyboard.is_pressed('s') and world[y+1][x] != WALL:
+        player["pos"] = (x, y+1)
+    if keyboard.is_pressed('d') and world[y][x+1] != WALL:
+        player["pos"] = (x+1, y)
+    if keyboard.is_pressed('space') and player["equipped"]:
+        player_action()
+    if keyboard.is_pressed('1'):
+        if is_player_in_ship():
+            try_dispense_seeds()
+        else:
+            try_equip_item(1)
+    if keyboard.is_pressed('2'):
+        try_equip_item(2)
+    if keyboard.is_pressed('3'):
+        try_equip_item(3)
+
+
+    # check for items
+    if player["pos"] in item_map:        
+        position = player["pos"]
+        item = item_map[position]
+        if item == SHIP:
+            pass
+        elif item in STACKABLE:
+            player["inventory"].setdefault(item, 0)
+            player["inventory"][item] += 1
+            del item_map[position]
+        else:
+            player["inventory"][item] = 1
+            del item_map[position]
+
+    # Auto-equip item
+    if player["equipped"] is None and player["inventory"]:
+        try_equip_item(1)
+
+    # grow the seeds
+    for i, seed_tuple in enumerate(seed_tracker):
+        x, y, seed_clock = seed_tuple
+        if clock - seed_clock < seed_growth_time:
+            continue        
+        index = seed_stages.index(world[y][x])
+        index += 1
+        if index < len(seed_stages) - 1:            
+            world[y][x] = seed_stages[index]
+        if index == len(seed_stages) - 1:
+            world[y][x] = BLANK
+            item_map[(x, y)] = POTATO
+            del seed_tracker[i]
+        else:
+            seed_tracker[i] = (x, y, clock)
+                
+    # decrement player hunger
+    if int(clock) % HUNGER_INTERVAL == 0:
+        player["hunger"] -= 1
+
+    # delete expired messages
+    for i, message_tuple in enumerate(feedback_messages):
+        message, message_clock = message_tuple
+        if clock - message_clock > MESSAGE_LIMIT:
+            del feedback_messages[i]
+
+
+def render():
+    global clock
+    global feedback_messages
+
+    world_scene = copy.deepcopy(world)
+    
+    # add player to scene
+    x, y = player["pos"]
+    world_scene[y][x] = PLAYER
+
+    # add ship to scene
+    x, y = ship_pos
+    world_scene[y][x] = SHIP
+
+    # add items to world scene
+    for position, item in item_map.items():
+        x, y = position
+        world_scene[y][x] = item
+        
+    # render game
+    for row in world_scene:
+        print(*row, sep='')
+        
+    # increment clock    
+    clock += 1
+    print("clock", clock)
+    
+    print_player_stats()
+    print_inventory()
+
+    if is_player_in_ship():
+        print_ship_menu()
+    
+    # display feedback messages
+    for message_tuple in feedback_messages:
+        message, _ = message_tuple
+        print(message)
+
+
+if __name__ == "__main__":
+    load_world("world.txt")
+    while True:
+        process()
+        render()
+        time.sleep(0.12)
+        os.system('cls')
